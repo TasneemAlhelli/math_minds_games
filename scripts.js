7ea0e15053ee8b6a8d1b0e96b9ba275c320c05d2
Tasneem <tasneem.jameel9@gmail.com>
2024-01-21 09:20:59 +0300

    Record Game Timer

--- scripts.js
+++ scripts.js
@@ -8,15 +8,42 @@ const advanceList = JSON.parse(window.localStorage.getItem('advanceEqs'))
 const eqSection = document.querySelector('.equation')
 const optionSection = document.querySelector('.options')
 const scoreEl = document.querySelector('#score')
+const timerEl = document.querySelector('#timer')
+
 let level = ''
 let list = []
 let correctAnswer = 0
 let score = 0
+let timer = 20
 
 /** FUNCTIONS */
+const setTimer = () => {
+  const mins = Math.floor(timer / 60)
+  let secs = timer % 60
+  let time = mins > 0 ? `${mins}:${secs}` : secs
+  timerEl.innerHTML = time
+}
+
+const startGame = () => {
+  // decrease time
+  timer--
+
+  // check if time over
+  if (timer < 0) {
+    console.log('game over')
+  }
+
+  // display timer
+  setTimer()
+}
+
 const evaluateAns = (btn) => {
   let answer = parseInt(btn.textContent)
   if (answer == correctAnswer) {
+    // increase timer
+    timer += 5
+    setTimer()
+
     // record score
     score += 10
     scoreEl.innerHTML = score
@@ -41,10 +68,20 @@ const getList = () => {
   } else if (score >= 100 && score < 300) {
     level = 'intermediate'
     list = intermediateList
+
+    // increase timer
+    timer += 10
+    setTimer()
+
     console.log('LEVEL CHANGED: INTERMEDIATE')
   } else {
     level = 'advance'
     list = advanceList
+
+    // increase timer
+    timer += 20
+    setTimer()
+
     console.log('LEVEL CHANGED: ADVANCE')
   }
 }
@@ -74,5 +111,7 @@ const displayEquation = () => {
 }
 getList()
 displayEquation()
+setTimer()
+setInterval(startGame, 1000)
 
 /** EVENT LISNTERS */
